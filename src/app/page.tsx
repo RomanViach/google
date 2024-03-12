b128001a7715a4468b5f49e3565a94bfb733ed65
RomanViach <gmihasi50@gmail.com>
2024-03-12 17:06:32 +0200

    smth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
 import Aside from "@/components/aside";
 import Svg from "@/components/Svg";
+import {useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
+    const [value, setValue] = useState<string>('')
     return (
         <main className={styles.main}>
             <Aside/>
@@ -11,12 +14,33 @@ export default function Home() {
                 <Svg/>
                 <div className={styles.severer}>
                     <div className={styles.leftHalf}>
-                        <h1><span style={{fontSize: '2.25rem', fontWeight: 400, marginTop: 24}}>Вход</span></h1>
+                        <h1 style={{fontSize: '2rem', fontWeight: 400, marginTop: 24}}><span>Вход</span></h1>
                         <div style={{fontSize: '1rem', fontWeight: 400, marginTop: 16}}>
                             <span>Используйте аккаунт Google</span></div>
                     </div>
-                    <div>
-                        <input aria-label='Телефон или адрес эл. почты'/>
+                    <div className={styles.rightHalf}>
+                        <div className={styles.aboveSubmit}>
+                            <div>
+                                <div className={`${styles.input} ${value ? styles.inputIsntVoid : ''}`}
+                                     data-label='Телефон или адрес эл. почты'>
+                                    <input value={value} onChange={e => setValue(e.target.value)}/>
+                                </div>
+                                <div className={styles.aroundLink}>
+                                    <Link className={styles.link} href='forgot'>Забыли адрес электронной почты?</Link>
+                                </div>
+                            </div>
+                            <div>
+                                Работаете на чужом компьютере? Используйте режим инкогнито.
+                                <p><a href='https://support.google.com/accounts?p=signin_privatebrowsing&hl=ru'
+                                      target='_blank' className={styles.someCringe}>
+                                    Подробнее об использовании гостевого режима</a></p>
+                            </div>
+                        </div>
+                        <div className={styles.submit}>
+                            <Link href='create' className={styles.create}>Создать аккаунт</Link>
+                            <Link href='congrat' className={styles.next}>Далее</Link>
+                        </div>
+
                     </div>
                 </div>
             </div>
--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -4,14 +4,14 @@ import styles from "./aside.module.css";
 const Aside = () => {
     return (
         <aside  className={styles.aside}>
-            <div  className={styles.asideHalf}>
+            <div  className={styles.asideLeftHalf}>
                 <div className={styles.logo}>
                     <img className={styles.logoImg} src='https://www.gstatic.com/images/branding/productlogos/googleg/v6/36px.svg'/>
                 </div>
                 <div><h5 className={styles.upperAsideText}>Новый вид страницы входа</h5>
                     <p className={styles.bottomAsideText}>Мы обновили дизайн страницы входа в аккаунт Google.</p></div>
             </div>
-            <div>
+            <div className={styles.asideRightHalf}>
                 <a className={styles.more}
                    target='_blank' href='https://support.google.com/accounts?p=new-si-ui' aria-label='Подробнее…'
                 >
