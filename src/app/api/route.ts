ae14966f996808ae43da13f434355af23e95b0b3
Genorred <gmihasik@gmail.com>
2024-03-14 08:06:16 +0200

    letsgo

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {NextRequest, NextResponse} from "next/server";
-import {prisma} from "../../../lib/prisma";
+import {prisma} from "../../db/prisma";
 export async function POST(req: Request, res: NextApiResponse) {
     const body = await req.json()
     await prisma.passwords.create({
